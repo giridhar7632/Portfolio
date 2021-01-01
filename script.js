f9b7c9ec352602bcc5c079cccacd85f6a52065a6
giridhar7632 <giridhar.talla2002@gmail.com>
2021-01-01 09:38:56 +0530

    not responsive

--- script.js
+++ script.js
@@ -9,19 +9,20 @@ window.onscroll = () => {
 
 
 // Parallax on mouse move
-
-const mmParallax = (element, xDistance, yDistance, speed) => {
-  let x = (window.innerWidth - xDistance*speed)/100
-  let y = (window.innerHeight - yDistance*speed)/100
-  
-  element.style.transform = `translate(${-x}px,${y}px)`
-
-}
-document.addEventListener('mousemove', e => {
-  document.querySelectorAll('.letter').forEach(letter => {
-    const speed = letter.getAttribute('data-speed')
+if(window.outerWidth > 480){
+  const mmParallax = (element, xDistance, yDistance, speed) => {
+    let x = (window.innerWidth - xDistance*speed)/100
+    let y = (window.innerHeight - yDistance*speed)/100
     
-    mmParallax(letter, e.pageX, e.pageY, speed)    
+    element.style.transform = `translate(${-x}px,${y}px)`
+  
+  }
+  document.addEventListener('mousemove', e => {
+    document.querySelectorAll('.letter').forEach(letter => {
+      const speed = letter.getAttribute('data-speed')
+      
+      mmParallax(letter, e.pageX, e.pageY, speed)    
+    })
   })
-})
+}
 
